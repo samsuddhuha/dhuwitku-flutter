4301bec4ab5aed2122c4ac66c28d6e384c852fb4
Samsud Dhuha <suddhuha@gmail.com>
2026-06-08 10:10:48 +0700

    Add cupertino import

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:dhuwitku/feature/splashscreen/splashscreen_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 void main() {
   runApp(const MyApp());
