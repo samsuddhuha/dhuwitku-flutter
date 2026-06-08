035affbf5cbaaac843016a9bcef2d3f4100323ef
Samsud Dhuha <suddhuha@gmail.com>
2026-06-08 10:27:49 +0700

    add max width for web app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:dhuwitku/feature/splashscreen/splashscreen_page.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
@@ -9,12 +10,34 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Dhuwitku',
+      builder: (context, child) {
+        if (!kIsWeb) {
+          return child!;
+        }
+
+        return LayoutBuilder(
+          builder: (context, constraints) {
+            if (constraints.maxWidth < 600) {
+              return child!;
+            }
+
+            return Container(
+              color: const Color(0xFFF5F5F5),
+              child: Center(
+                child: SizedBox(
+                  width: 430,
+                  child: Material(color: Colors.white, child: child!),
+                ),
+              ),
+            );
+          },
+        );
+      },
       theme: ThemeData(
         useMaterial3: true,
         pageTransitionsTheme: PageTransitionsTheme(
@@ -34,3 +57,32 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Dhuwitku',
+//       theme: ThemeData(
+//         useMaterial3: true,
+//         pageTransitionsTheme: PageTransitionsTheme(
+//           builders: {
+//             TargetPlatform.android: CupertinoPageTransitionsBuilder(),
+//             TargetPlatform.iOS: CupertinoPageTransitionsBuilder(),
+//           },
+//         ),
+//         fontFamily: 'HostGrotesk',
+//         textTheme: const TextTheme(
+//           bodyLarge: TextStyle(fontWeight: FontWeight.w400),
+//           bodyMedium: TextStyle(fontWeight: FontWeight.w400),
+//           titleMedium: TextStyle(fontWeight: FontWeight.w600),
+//         ),
+//       ),
+//       home: const SplashScreenPage(),
+//     );
+//   }
+// }
